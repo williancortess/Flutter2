6c9c75a8f45e60d13b098c09a2527499978f9efc
williancortess <williankzn@gmail.com>
2020-08-30 15:29:02 -0300

    Insert Sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:bytebank2/database/app_database.dart';
+import 'package:bytebank2/models/contact.dart';
 import 'package:flutter/material.dart';
+
+import 'screens/dashboard.dart';
 
 void main() {
   runApp(Bytebank2App());
+  findAll().then((contacts) => debugPrint(contacts.toString()));
 }
 
 class Bytebank2App extends StatelessWidget {
@@ -17,54 +22,6 @@ class Bytebank2App extends StatelessWidget {
             textTheme: ButtonTextTheme.primary,
           )),
       home: Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,62 @@
+import 'package:bytebank2/models/contact.dart';
+import 'package:bytebank2/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactList extends StatelessWidget {
+  final List<Contact> contacts = List();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          )
+              .then(
+                (newContact) => debugPrint(newContact.toString()),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+}
